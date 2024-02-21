03ff978f7b22cf2db8093a02c1717bf75b053c1c
supriyahatele <niyahatele70@gmail.com>
2024-02-21 21:42:47 +0530

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 import './App.css'
+import ConfirmPage from './components/ConfirmPage'
 
 function App() {
  
   return (
   <div className='App'>
-   
+   <ConfirmPage/>
   </div>
   )
 }
 
 export default App
+
+
