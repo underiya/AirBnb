df422feea98e0d0a67fa570f5e06b49077f17875
Deepak Underiya <underiyadeepak@gmail.com>
2024-02-23 13:44:03 +0530

    update redux reducer

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,3 +1,11 @@
+import {
+  GET_CART_SUCCESS,
+  GET_LOCATIONS_FAILURE,
+  GET_LOCATIONS_REQUEST,
+  GET_LOCATIONS_SUCCESS,
+  GET_WISHLIST_SUCCESS,
+} from "./actionType";
+
 const initialState = {
   isLoading: false,
   isError: false,
@@ -6,12 +14,20 @@ const initialState = {
 
 export const locationReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOCATIONS_REQUEST:
+      return { ...state, isLoading: true };
+    case GET_LOCATIONS_FAILURE:
+      return { ...state, isLoading: false, isError: true };
+    case GET_LOCATIONS_SUCCESS:
+      return { ...state, isLoading: false, products: action.payload };
     default:
       return state;
   }
 };
 export const wishlistReducer = (state = [], action) => {
   switch (action.type) {
+    case GET_WISHLIST_SUCCESS:
+      return [...state, action.payload];
     default:
       return state;
   }
@@ -19,6 +35,8 @@ export const wishlistReducer = (state = [], action) => {
 
 export const cartReducer = (state = [], action) => {
   switch (action.type) {
+    case GET_CART_SUCCESS:
+      return [...state, action.payload];
     default:
       return state;
   }
