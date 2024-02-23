f0cebd9baba0f45a5ec5faf3ce5786c2ff5898ae
renuka <renukasjagrwal@gmail.com>
2024-02-23 23:05:25 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Footer from "./Component/Footer";
 import "boxicons";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 import "boxicons/css/boxicons.min.css";
 import Navbar from "./Component/Navbar";
-import Details from "./Component/Details";
+
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Details/>
+  
+   
+    
       <Footer />
    
     </div>
