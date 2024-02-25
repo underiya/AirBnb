5c285113e52e56d01d4bc21de582fc9609b03d57
Yawer091 <yaweratique90@gmail.com>
2024-02-26 00:09:20 +0530

    day5last

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import "./App.css";
 import Footer from "./Component/Footer";
 import "boxicons";
 import "boxicons/css/boxicons.min.css";
-import Navbar from "./Component/Navbar";
-import CardCarousel from "./Component/CardCarousel";
 import AllRoutes from "./Component/AllRoutes";
-
 import Header from "./Component/Header";
-
-import SideNavbar from "./Admin/SideNavbar";
-import Home from "./Component/Home";
 function App() {
   return (
     <>
